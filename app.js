e65943ccc3a3a6cc0772a12f586d7b797347b468
Pranav Kunapuli <pranavtkunapuli@gmail.com>
2015-08-10 11:43:22 -0400

    Reorganization

---
+++ app.js
@@ -0,0 +1,6 @@
+var vidfusion = angular.module("vidfusion", []);
+var ref = new Firebase("https://blistering-fire-2832.firebaseio.com/");
+
+vidfusion.config(function() {
+
+});
---
+++ app/controllers/login.js
@@ -0,0 +1,40 @@
+angular.module("vidfusion").controller("LoginController", function($scope) {
+    $scope.client_id = "1037941277521-25nd37qbdsb5daqlm1fcsh7fub418sfs.apps.googleusercontent.com";
+    var scopes = ['https://www.googleapis.com/auth/youtube'];
+
+    $scope.googleLogin = function() {
+        gapi.auth.authorize({client_id: $scope.clientId, scope: scopes, immediate: true}, handleAuthResult);
+    };
+
+    function handleAuthResult(authResult) {
+      if (authResult && !authResult.error) {
+        loadAPI();
+      } else {
+        console.log("authResult returned an error")
+      }
+    }
+
+    function handleAuthClick(event) {
+      gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+      return false;
+    }
+
+    function loadAPI() {
+        gapi.client.load("youtube", "v3", function() {
+            console.log("Youtube Loaded Successfully");
+        });
+    }
+
+    // Login with Google using Firebase OAuth
+    $scope.login = function() {
+	    ref.authWithOAuthPopup("google", function(error, authData) {
+    		if(!error) {
+    			console.log("Login Successful");
+    			$scope.user = authData;
+    		} else {
+    			console.log("Error with Google Authentication");
+    		}
+        }
+	});
+
+});
