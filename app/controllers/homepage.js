e5bbc08361d6a6c0392faa614d5a45e75e018ac2
Pranav Kunapuli <pranavtkunapuli@gmail.com>
2015-08-18 15:59:46 -0400

    Trying to list uploaded videos

--- app/controllers/homepage.js
+++ app/controllers/homepage.js
@@ -1,3 +1,30 @@
 angular.module("vidfusion").controller("HomePageController", function($scope, $location, Firebase) {
     var ref = Firebase;
+
+    $scope.videos = {};
+
+    $scope.requestUserUploads= function() {
+        var userUploadsRequest = gapi.client.youtube.channels.list({
+            mine: true,
+            part: "contentDetails"
+        });
+
+        userUploadsRequest.execute(function(response) {
+            $scope.playlistID = response.result.items[0].contentDetails.relatedPlaylists.uploads;
+            requestVideoPlaylist();
+        });
+
+        var requestVideoPlaylist = function() {
+            var playlistRequest = gapi.client.youtube.playlistItems.list({
+                playlistId: $scope.playlistID,
+                part: "snippet",
+                maxResults: 10
+            });
+
+            playlistRequest.execute(function(response) {
+                var playlistItems = response.result.items;
+                console.log(JSON.stringify(playlistItems, null, 2));
+            });
+        };
+    }
 });
